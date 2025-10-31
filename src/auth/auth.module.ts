20fcf5344d5812b310ac5eef1747086a0bfc4239
PatoDckY <jesusf1705dck@gmail.com>
2025-10-31 16:05:41 -0600

    v5

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,21 +4,22 @@ import { AuthController } from './auth.controller';
 import { UsuariosModule } from '../usuarios/usuarios.module';
 import { JwtModule } from '@nestjs/jwt';
 import { ConfigModule, ConfigService } from '@nestjs/config';
+import { JwtStrategy } from './jwt.strategy'; // 👈 importa la estrategia
 
 @Module({
   imports: [
-    ConfigModule.forRoot({ isGlobal: true }), // 👈 Para leer .env
+    ConfigModule.forRoot({ isGlobal: true }),
     UsuariosModule,
     JwtModule.registerAsync({
       imports: [ConfigModule],
       inject: [ConfigService],
       useFactory: (configService: ConfigService) => ({
-        secret: configService.get<string>('JWT_SECRET'), // 👈 ahora seguro existe
+        secret: configService.get<string>('JWT_SECRET'),
         signOptions: { expiresIn: '1h' },
       }),
     }),
   ],
-  providers: [AuthService],
+  providers: [AuthService, JwtStrategy], // 👈 agrega aquí la estrategia
   controllers: [AuthController],
 })
 export class AuthModule {}
